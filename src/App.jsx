cc6539ccd645f8efdc08d9ceb830e2a10b43abc7
abircu <mdabircse4873@gmail.com>
2023-06-13 00:49:34 +0600

    This is hero section banner page

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Hero from "./componend/Hero";
+import Navigation from "./componend/navigation";
+const App=()=>{
+return(
+    <div>
+        <Navigation/>
+        <Hero/>
+    </div>
+);
+};
+export default App;
---
+++ src/componend/navigation.jsx
@@ -0,0 +1,20 @@
+
+const Navigation=()=>{
+
+ return(
+    <nav className="container">
+        <div className="logo">
+            <img src="images/nike.png" alt="" />
+        </div>
+        <ul className="list-item">
+            <li href="#">Home</li>
+            <li href="#">About</li>
+            <li href="#">Service</li>
+            <li href="#">Contact</li>
+        </ul>
+        <button>login</button>
+    </nav>
+
+)
+}
+export default Navigation;
